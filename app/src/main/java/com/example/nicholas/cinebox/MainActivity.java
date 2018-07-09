b2935b0d38fe28158431af802979a29b36deccff
nicholastqs <nicholas-tan@hotmail.com>
2018-07-09 20:56:09 +0800

    New

--- app/src/main/java/com/example/nicholas/cinebox/MainActivity.java
+++ app/src/main/java/com/example/nicholas/cinebox/MainActivity.java
@@ -35,6 +35,7 @@ import android.widget.TextView;
 import com.example.nicholas.cinebox.activities.BaseActivity;
 import com.example.nicholas.cinebox.activities.WelcomeActivity;
 import com.example.nicholas.cinebox.fragments.FavoritesFragment;
+import com.example.nicholas.cinebox.fragments.HomeFragment;
 import com.example.nicholas.cinebox.fragments.RatedFragment;
 import com.example.nicholas.cinebox.fragments.RecommendedFragment;
 import com.example.nicholas.cinebox.fragments.WatchListFragment;
@@ -118,10 +119,10 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         int id = item.getItemId();
         if (id == R.id.nav_home) {
             mFragmentTransaction = mFragmentManager.beginTransaction();
-            mFragmentTransaction.replace(R.id.containerView, new WatchListFragment()).addToBackStack(Constants.NAV_ITEM_ID).commit();
+            mFragmentTransaction.replace(R.id.containerView, new HomeFragment()).addToBackStack(Constants.NAV_ITEM_ID).commit();
         } else if (id == R.id.nav_watchlist) {
             mFragmentTransaction = mFragmentManager.beginTransaction();
-            mFragmentTransaction.replace(R.id.containerView, RatedFragment.newInstance()).commit();
+            mFragmentTransaction.replace(R.id.containerView, WatchListFragment.newInstance()).commit();
         } else if (id == R.id.nav_rated) {
             mFragmentTransaction = mFragmentManager.beginTransaction();
             mFragmentTransaction.replace(R.id.containerView, RatedFragment.newInstance()).commit();
